2ea2e8712e84d8e3fa19cd21a8445272e51b8b75
ddknight <cc199026@gmail.com>
2019-01-18 06:40:41 +0800

    first commit

---
+++ src/main/java/cc/ddknight/app/swagger/apidoc/helper/Application.java
@@ -0,0 +1,29 @@
+package cc.ddknight.app.swagger.apidoc.helper;
+
+import io.github.swagger2markup.GroupBy;
+import io.github.swagger2markup.Language;
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+        // swagger-ui.html页面中能找到此链接
+        Swagger2MarkupConverter.from(new URL("http://localhost:9601/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("./docs/asciidoc/generated/all"));
+    }
+
+}
